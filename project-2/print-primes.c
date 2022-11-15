fce13f4fa8c0721ecdb67e583a1bdd94ad417af0
Karim-Mttk <89412432+karim-mttk@users.noreply.github.com>
2022-11-15 23:21:48 +0100

    Add files via upload

---
+++ project-2/print-primes.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define COLUMNS 6
+
+
+void print_primes(int n){
+  // Should print out all prime numbers less than 'n'
+  // with the following formatting. Note that
+  // the number of columns is stated in the define
+  // COLUMNS
+
+  printf("%10d ", 2);
+  printf("%10d ", 3);
+  printf("%10d ", 5);
+  printf("%10d ", 7);
+  printf("%10d ", 11);
+  printf("%10d ", 13);
+  printf("\n");
+  printf("%10d ", 17);
+  printf("%10d ", 19);
+
+  printf("\n");
+}
+
+// 'argc' contains the number of program arguments, and
+// 'argv' is an array of char pointers, where each
+// char pointer points to a null-terminated string.
+int main(int argc, char *argv[]){
+  if(argc == 2)
+    print_primes(atoi(argv[1]));
+  else
+    printf("Please state an interger number.\n");
+  return 0;
+}
+
+ 
