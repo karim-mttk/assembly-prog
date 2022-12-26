49e9a9b063c2127535a15c2a902f492f7905fc0d
Karim-Mttk <89412432+karim-mttk@users.noreply.github.com>
2022-12-26 20:06:16 +0100

    Update ptr_add.c

--- exercises/pointers/ptr_add.c
+++ exercises/pointers/ptr_add.c
@@ -2,19 +2,19 @@
 
 int main(){
 
-int num1, num2, sum;
-int *x;
-int *y;
+  int num1, num2, sum;
+  int *x;
+  int *y;
 
-x = &num1;
-y = &num2;
+  x = &num1;
+  y = &num2;
 
 
-printf("Enter two nums: \n");
-scanf("%d%d", x, y);
+  printf("Enter two nums: \n");
+  scanf("%d%d", x, y);
 
-sum = *x + *y;
-printf("The sum is: %d", sum); 
+  sum = *x + *y;
+  printf("The sum is: %d", sum); 
 
 return 0;
 }
